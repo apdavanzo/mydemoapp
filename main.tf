5e16e269c70bc887854c98ceb35754e26388813a
Adam Cavaliere <adam@Adams-MacBook-Pro-2.local>
2018-02-25 22:09:03 -0800

    first

---
+++ main.tf
@@ -0,0 +1,20 @@
+module "network" {
+  source  = "atlas.hashicorp.com/fpollock-demo/network/aws"
+  version = "0.0.9"
+}
+
+provider "aws" {
+    region = "${var.aws_region}"
+}
+
+resource "aws_instance" "foo" {
+  ami           = "${var.ami_id}"
+  instance_type = "${var.instance_type}"
+  availability_zone = "${var.aws_region}a"
+
+  tags {
+    owner = "Adam"
+    TTL = 1
+  }
+  subnet_id = "${module.network.demo_subnet_id}"
+}
